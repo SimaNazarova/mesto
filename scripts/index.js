6b51a092ab3f66cc7de1107062eff3eeda40fa1e
Serafima Nazarova <sima.nazarova@gmail.com>
2020-08-30 11:20:49 +0300

    на проверку

---
+++ scripts/index.js
@@ -0,0 +1,46 @@
+let popup = document.querySelector('.popup');
+
+let popupCloseButton = popup.querySelector('.popup__close');
+let formElement =  popup.querySelector('.popup__form');
+
+let nameInput = formElement.querySelector('.popup__form-name');
+let jobInput = formElement.querySelector('.popup__form-job');
+
+let profile = document.querySelector('.profile');
+
+let popupProfileEditButton = profile.querySelector('.profile__edit-button');
+let profileName = profile.querySelector('.profile__name');
+let profileNameAbout = profile.querySelector('.profile__name-about');
+
+
+//функция для отображения pop-up
+let popupToggle = function () {
+  popup.classList.toggle('popup_opened');
+  if (popup.classList.contains('popup_opened')){
+    nameInput.value = profileName.textContent;
+    jobInput.value = profileNameAbout.textContent;
+  }
+}
+
+//функция для закрытия pop-up по клику на затемнении
+let popupCloseByClickOnOverlay = function(event) {
+  if (event.target !== event.currentTarget) {
+    return
+  }
+  popupToggle(event)
+}
+
+//Обработчик «отправки» формы
+function formSubmitHandler (event) {
+    event.preventDefault();
+    profileName.textContent = nameInput.value;
+    profileNameAbout.textContent = jobInput.value;
+    popupToggle();
+}
+
+
+popupProfileEditButton.addEventListener('click', popupToggle);
+popupCloseButton.addEventListener('click', popupToggle);
+popup.addEventListener('click', popupCloseByClickOnOverlay);
+formElement.addEventListener('submit', formSubmitHandler);
+
